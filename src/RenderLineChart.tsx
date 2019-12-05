a982d27341d76103829e2dc025b975b69517a11c
00kubo <glenfarclascript@gmail.com>
2019-12-06 03:09:58 +0900

    データ追加, 系列装飾

--- src/RenderLineChart.tsx
+++ src/RenderLineChart.tsx
@@ -1,9 +1,41 @@
 import React from 'react';
-import { LineChart, Line } from 'recharts';
-const data = [{name: 'Page A', uv: 500, pv: 2000, amt: 1000}, {name: 'Page B', uv: 700, pv: 3200, amt: 2400}, {name: 'Page C', uv: 400, pv: 2400, amt: 1800}];
+import { LineChart, Line, CartesianGrid, Tooltip } from 'recharts';
+const data = [
+  {clk: 17, tw: 20, fb: 1000},
+  {clk: 15, tw: 40, fb: 2400},
+  {clk: 18, tw: 60, fb: 1000},
+  {clk: 53, tw: 80, fb: 2400},
+  {clk: 20, tw: 100, fb: 1000},
+  {clk: 23, tw: 120, fb: 2400},
+  {clk: 20, tw: 20, fb: 1000},
+  {clk: 22, tw: 40, fb: 2400},
+  {clk: 26, tw: 60, fb: 1000},
+  {clk: 23, tw: 120, fb: 2400},
+  {clk: 48, tw: 20, fb: 1000},
+  {clk: 25, tw: 40, fb: 2400},
+  {clk: 27, tw: 60, fb: 1000},
+  {clk: 29, tw: 80, fb: 2400},
+  {clk: 26, tw: 100, fb: 1000},
+  {clk: 29, tw: 120, fb: 2400},
+  {clk: 27, tw: 20, fb: 1000},
+  {clk: 7, tw: 40, fb: 2400},
+  {clk: 27, tw: 60, fb: 1000},
+  {clk: 20, tw: 120, fb: 2400},
+  {clk: 23, tw: 20, fb: 1000},
+  {clk: 24, tw: 40, fb: 2400},
+  {clk: 22, tw: 60, fb: 1000},
+  {clk: 23, tw: 80, fb: 2400},
+  {clk: 22, tw: 100, fb: 1000},
+  {clk: 28, tw: 120, fb: 2400},
+  {clk: 23, tw: 20, fb: 1000},
+  {clk: 29, tw: 40, fb: 2400},
+  {clk: 32, tw: 60, fb: 1000}
+];
 
 export default () => (
-  <LineChart width={400} height={400} data={data}>
-    <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+  <LineChart width={600} height={300} data={data} margin={{ top: 20, right: 20, bottom: 5, left: 0 }}>
+    <Line dot={false} type="linear" dataKey="clk" stroke="#DAA520" strokeWidth={2}/>
+    <CartesianGrid vertical={false} stroke="#EEE" />
+    <Tooltip />
   </LineChart>
 );
